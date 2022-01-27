632d2fcd5001f405ac7a84c1bdbdb87a68248af4
Tore Brede <tore.brede@gmail.com>
2022-01-27 21:37:19 +0100

    Looking at a test

--- websockets/src/test/java/com/kjipo/WebSocketClientTest.java
+++ websockets/src/test/java/com/kjipo/WebSocketClientTest.java
@@ -1,41 +1,53 @@
 package com.kjipo;
 
 
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
 import org.springframework.web.reactive.socket.client.WebSocketClient;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
-@SpringBootTest //(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration(
+        // TODO Figure out why the data source does not get set up during testing
+        exclude = {
+                R2dbcAutoConfiguration.class,
+        }
+)
 public class WebSocketClientTest {
 
-//    @LocalServerPort
-//    private int port;
+    @LocalServerPort
+    private int port;
 
 
     @Test
     public void clientGetsDataTest() {
-//        WebSocketClient client = new ReactorNettyWebSocketClient();
-//        client.execute(
-//                        URI.create("ws://localhost:" +port +"/event-emitter"),
-//                        session -> session.send(
-//                                        Mono.just(session.textMessage("event-spring-reactive-client-websocket")))
-//                                .thenMany(session.receive().
-//                                        map(WebSocketMessage::getPayloadAsText)
-//                                        .log())
-//                                .then())
-//                .block(Duration.ofSeconds(10L));
+        AtomicInteger messageCounter = new AtomicInteger();
+        WebSocketClient client = new ReactorNettyWebSocketClient();
+        client.execute(
+                URI.create("ws://localhost:" + port + "/event-emitter"),
+                session -> session.send(
+                                Mono.just(session.textMessage("event-spring-reactive-client-websocket")))
+                        .thenMany(session.receive().
+                                map(WebSocketMessage::getPayloadAsText)
+                                .log()
+                                .map((logMessage) -> {
+                                    messageCounter.getAndIncrement();
+                                    return logMessage;
+                                })).then()).subscribe();
 
-        System.out.println("Test23");
-
-
+        Awaitility.await().atMost(5, TimeUnit.SECONDS).untilAtomic(messageCounter, greaterThanOrEqualTo(3));
     }
 
 }
